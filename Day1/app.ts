8feb88ca4f7b5800d42076bb3b0b53ecb093680c
rkapitonov <aootee@gmail.com>
2019-01-22 17:00:24 +0300

    day1

--- Day1/app.ts
+++ Day1/app.ts
@@ -2,7 +2,9 @@ import { read } from "./console";
 
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    console.log('vvedite imya');
+    const str = await read();
+    console.log('Hello,', str);
 }
 
 main();
