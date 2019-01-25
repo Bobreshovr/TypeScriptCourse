517000acb0bf57ed8b547b04e477e4f2de062c7f
artem-telitsyn <artem.telitsyn@gmail.com>
2019-01-25 14:45:48 +0300

    day2

--- Day2/app.ts
+++ Day2/app.ts
@@ -1,8 +1,18 @@
 import { read } from "./console";
 
-async function main(): Promise<void>
-{
+async function main(): Promise<void> {
     //Здесь необходимо писать реализацию
+    console.log('Введи символ');
+    const str = await read();
+    if (str.length === 1) {
+        if ((str <= 'z' && str >= 'a') || (str <= 'Z' && str >= 'A')) {
+            console.log('Это символ латинского алфавита');
+        } else {
+            console.log('Не символ латинского алфавита');
+        }
+    } else {
+        console.log('Надо было ввести одну букву');
+    }
 }
 
 main();
