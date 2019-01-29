80e4234a12f3fb2765b744cf53e746df1ea6b21b
ibulygin <38670453+ibulygin@users.noreply.github.com>
2019-01-29 17:11:21 +0300

    class work

--- Day3/app.ts
+++ Day3/app.ts
@@ -1,8 +1,19 @@
-import { read } from "./console";
+
 
 async function main(): Promise<void>
 {
     //Здесь необходимо писать реализацию
+    var phrase = 'Простите, сэр. Я Гарри, сэр. Гарри Поттер';
+    var arr = phrase.split(' ');
+    console.log(arr);
+    var arrInvers = [];
+    var index;
+    for(var i = arr.length; i > 0; i--){
+        index = arr.length-i;
+        arrInvers[i] = arr[index];
+    }
+    var resultPrase = arrInvers.join(' ');
+    console.log(resultPrase);
 }
 
 main();
