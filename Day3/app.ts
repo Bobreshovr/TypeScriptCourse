1e0525411d6073a6943e0309f7f587b46d499bd1
artem-telitsyn <artem.telitsyn@gmail.com>
2019-01-29 15:58:23 +0300

    dsf

--- Day3/app.ts
+++ Day3/app.ts
@@ -3,6 +3,15 @@ import { read } from "./console";
 async function main(): Promise<void>
 {
     //Здесь необходимо писать реализацию
+    console.log('Введи символ');
+    const str = await read();
+    var splitStr = str.split(" ");
+    var newStr : string[];
+    for (var i = 0; i < splitStr.length; i++) {
+        console.log(splitStr.length);
+        newStr[splitStr.length] = splitStr[i];
+    }
+    console.log(newStr);
 }
 
 main();
