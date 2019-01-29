aca2f6b4fca20bfec261e6a1464dc432600fdd4f
merryhangedman <merryhangedman@gmail.com>
2019-01-29 16:00:01 +0300

    Day3 Draft

--- Day3/app.ts
+++ Day3/app.ts
@@ -2,7 +2,21 @@ import { read } from "./console";
 
 async function main(): Promise<void>
 {
-    //Здесь необходимо писать реализацию
+    console.log('Введите строку для инверсии');
+    const defaultString= await read();
+
+    var splittedString= defaultString.split(" ");
+    console.log(splittedString);
+    
+    for ( var i=0;i < splittedString.length ; i++){
+
+    }
+    
+    // Вычисляем размерность массива splittedString
+    // Запускаем цикл длиной в размерность массива
+    // Последний член массива становится превым
+    // Далее конкатенируем до размерности массива -1
+    // Выводим 
 }
 
 main();
