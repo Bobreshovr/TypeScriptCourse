24cfae3c7daefaac669f63572b104bd2876914f0
a.telitsyn <a.telitsyn@tinkoff.ru>
2019-01-31 10:58:33 +0300

    homeWorkDay3

--- Day3/app.ts
+++ Day3/app.ts
@@ -1,17 +1,25 @@
 import { read } from "./console";
 
-async function main(): Promise<void>
-{
+async function main(): Promise<void> {
     //Здесь необходимо писать реализацию
-    console.log('Введи символ');
+    console.log('Введи строку');
+
     const str = await read();
     var splitStr = str.split(" ");
-    var newStr : string[];
-    for (var i = 0; i < splitStr.length; i++) {
-        console.log(splitStr.length);
-        newStr[splitStr.length] = splitStr[i];
+    var strLength: number = splitStr.length;
+
+    var new2Str: string = '';
+
+    for (var i = strLength - 1; i >= 0; i--) {
+        if (i === 0) {
+            new2Str += splitStr[i];
+            continue;
+        }
+        new2Str += splitStr[i] + " ";
+
     }
-    console.log(newStr);
+
+    console.log(new2Str);
 }
 
 main();
