47e69ce0800ddf7a5a27db81cd8b250c5cd544c2
Nikita D <merryhangedman@gmail.com>
2019-02-10 19:53:31 +0300

    Day5 Homework draft

---
+++ Day5/timer.ts
@@ -0,0 +1,8 @@
+var laptime:any = 0;
+document.getElementById('start').onclick = function(){laptime = new Date().getTime()}
+document.getElementById('stop').onclick = function(){laptime = 0}
+
+var interval = setInterval(function(){
+    if (laptime==0) return;
+    document.getElementById('laptime').innerHTML = (new Date().getTime()-laptime)/1000 
+},100);
