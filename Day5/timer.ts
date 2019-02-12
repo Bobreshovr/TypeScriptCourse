4213609103655a4f18f51e3e112d5318acb7f110
Nikita D <merryhangedman@gmail.com>
2019-02-12 11:26:37 +0300

    Use addEventListener istead of onClick

--- Day5/timer.ts
+++ Day5/timer.ts
@@ -1,8 +1,8 @@
-var laptime:any = 0;
-document.getElementById('start').onclick = function(){laptime = new Date().getTime()}
-document.getElementById('stop').onclick = function(){laptime = 0}
-
-var interval = setInterval(function(){
-    if (laptime==0) return;
-    document.getElementById('laptime').innerHTML = (new Date().getTime()-laptime)/1000 
-},100);
+var laptime = 0;
+document.getElementById('start').addEventListener('click', function () { laptime = new Date().getTime() });
+document.getElementById('stop').addEventListener('click', function () { laptime = 0; });
+var interval = setInterval(function () {
+    if (laptime == 0)
+        return;
+    document.getElementById('laptime').innerHTML = (new Date().getTime() - laptime) / 1000;
+}, 100);
